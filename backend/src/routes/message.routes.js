1ef3773051ee115ead11b18a17b8eed759e93575
Dvwnmark <xcmark141@gmail.com>
2025-10-22 22:12:18 +0700

    messages route

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { 
     getAllContacts, 
+    getChatPartners, 
     getMessagesByUserId,
     sendMessage, 
 } from "../controllers/message.controller.js";
@@ -14,7 +15,7 @@ const router = express.Router();
 router.use(arcjetProtection, protectRoute);
 
 router.get("/contacts", getAllContacts);
-//router.get("/chats", getChatPartners);
+router.get("/chats", getChatPartners);
 router.get("/:id", getMessagesByUserId);
 router.post("/send/:id", sendMessage);
 

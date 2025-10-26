63a63099d3523ba29af355df96b14d686b72d5ee
Dvwnmark <xcmark141@gmail.com>
2025-10-26 16:58:34 +0700

    fe codebase setup

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LoginPage() {
+    const { authUser, isLoading, login } = useAuthStore();
+  
+  return (
+    <div>
+        LoginPage
+    </div>
+  )
+}
+
+export default LoginPage

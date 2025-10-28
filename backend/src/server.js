072bfe54e741cae2859ac4f52e6408d86d065d82
Dvwnmark <xcmark141@gmail.com>
2025-10-28 22:35:22 +0700

    signup-implemnted on the fe

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,12 @@
 import express from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
+import path from "path";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.routes.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
-
-
 
 const app = express();
 const __dirname = path.resolve();
@@ -15,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); //req.body
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
